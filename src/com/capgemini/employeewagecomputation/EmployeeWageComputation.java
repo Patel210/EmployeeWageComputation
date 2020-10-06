165e3c68e550a42416f584952c56f2175be25685
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 16:55:08 +0530

    Added: Monthly Employee Wage calculator based on type of employee presence

--- src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
@@ -4,28 +4,34 @@ public class EmployeeWageComputation {
 	private static final int IS_FULL_TIME = 2;
 	private static final int WAGE_PER_HOUR = 20;
 	private static final int IS_PART_TIME = 1;
-	
-	
+	private static final int NO_OF_WORKING_DAYS = 20;
+
 	public static void main(String[] args) {
-		
+
 		int workHour = 0;
-		int empCheck = (int) Math.floor(Math.random()*10) % 3;
 		int empWage = 0;
-		
-		switch (empCheck) {
-		case IS_PART_TIME:
-			workHour = 4; 
-			break;
-		
-		case IS_FULL_TIME:
-			workHour = 8;
-			break;
-		
-		default:
-			workHour = 0;
+		int totalWage = 0;
+
+		for (int days = 0; days < NO_OF_WORKING_DAYS; days++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+			switch (empCheck) {
+			case IS_PART_TIME:
+				workHour = 4;
+				break;
+
+			case IS_FULL_TIME:
+				workHour = 8;
+				break;
+
+			default:
+				workHour = 0;
+			}
+			empWage = workHour * WAGE_PER_HOUR;
+			totalWage += empWage;
+		}
+
+		System.out.println("Daily Employee Wage: " + totalWage);
 	}
-		
-		empWage = workHour * WAGE_PER_HOUR;
-		System.out.println("Daily Employee Wage: " + empWage);
-	}
+
 }
