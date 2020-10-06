0a72f1dd0de06096cfb0466b6db3428961118231
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 17:46:42 +0530

    Modified: calculateWage method to calculate employee wage for different companies with diff working parameters

--- src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
@@ -2,50 +2,67 @@ package com.capgemini.employeewagecomputation;
 
 public class EmployeeWageComputation {
 	private static final int IS_FULL_TIME = 2;
-	private static final int WAGE_PER_HOUR = 20;
 	private static final int IS_PART_TIME = 1;
-	private static final int NO_OF_WORKING_DAYS = 20;
-	private static final int TOTAL_FIXED_WORKING_HOURS = 100;
 	private static final int HOURS_FULL_TIME = 8;
 	private static final int HOURS_PART_TIME = 4;
 
 	/**
 	 * To compute wage
 	 */
-	public int computeWage() {
+	public static int computeWage(String companyName, int wagePerHour, int noOfWorkingDays,
+			int totalFixedWorkingHours) {
 
 		int totalWage = 0, totalHours = 0;
-		for (int days = 0; days < NO_OF_WORKING_DAYS && totalHours < TOTAL_FIXED_WORKING_HOURS; days++) {
+		for (int days = 0; days < noOfWorkingDays && totalHours < totalFixedWorkingHours; days++) {
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-
-			switch (empCheck) {
-			case IS_PART_TIME:
-				totalHours += HOURS_PART_TIME;
-				totalWage += HOURS_PART_TIME * WAGE_PER_HOUR;
-
-				if (totalHours > TOTAL_FIXED_WORKING_HOURS) { // To Check whether total hours has exceeded 100 hours
-																// limit
-					totalWage += (totalHours - TOTAL_FIXED_WORKING_HOURS) * WAGE_PER_HOUR;
-				}
-				break;
-
-			case IS_FULL_TIME:
-				totalHours += HOURS_FULL_TIME;
-				totalWage += HOURS_FULL_TIME * WAGE_PER_HOUR;
-
-				if (totalHours > TOTAL_FIXED_WORKING_HOURS) { // To Check whether total hours has exceeded 100 hours
-																// limit
-					totalWage += (totalHours - TOTAL_FIXED_WORKING_HOURS) * WAGE_PER_HOUR;
-				}
-				break;
+			totalWage += getDailyWage(wagePerHour);
+			totalHours += getDailyHours(wagePerHour);
+			if (totalHours > totalFixedWorkingHours) {
+				totalWage -= (totalHours - totalFixedWorkingHours) * wagePerHour;
 			}
 		}
 		return totalWage;
 	}
 
+	/**
+	 * @param To get Daily Wage Expense
+	 * @return
+	 */
+	public static int getDailyWage(int wagePerHour) {
+		int dailyWage;
+		switch ((int) Math.floor(Math.random() * 10) % 3) {
+		case IS_PART_TIME:
+			dailyWage = HOURS_PART_TIME * wagePerHour;
+			return dailyWage;
+
+		case IS_FULL_TIME:
+			dailyWage = HOURS_FULL_TIME * wagePerHour;
+			return dailyWage;
+
+		default:
+			return 0;
+		}
+	}
+
+	/**
+	 * @param To get Daily working hour
+	 * @return
+	 */
+	public static int getDailyHours(int wagePerHour) {
+		switch ((int) Math.floor(Math.random() * 10) % 3) {
+		case IS_PART_TIME:
+			return HOURS_PART_TIME;
+		case IS_FULL_TIME:
+			return HOURS_FULL_TIME;
+
+		default:
+			return 0;
+		}
+	}
+
 	public static void main(String[] args) {
-		EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation();
-		System.out.println(employeeWageComputation.computeWage());
+		System.out.println(computeWage("Codesity", 100, 20, 100));
+		System.out.println(computeWage("ePW", 50, 22, 120));
 	}
 
 }
