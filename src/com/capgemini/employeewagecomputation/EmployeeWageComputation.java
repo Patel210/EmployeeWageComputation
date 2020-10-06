7385e4c12fd9ffbfcbe6f6dfcced8c45a2960bee
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 17:03:33 +0530

    Added: Monthly or completion of total fixed hours - Employee Wage calculator based on type of employees

--- src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
@@ -5,23 +5,31 @@ public class EmployeeWageComputation {
 	private static final int WAGE_PER_HOUR = 20;
 	private static final int IS_PART_TIME = 1;
 	private static final int NO_OF_WORKING_DAYS = 20;
+	private static final int TOTAL_FIXED_WORKING_HOURS = 100;
 
 	public static void main(String[] args) {
 
 		int workHour = 0;
 		int empWage = 0;
 		int totalWage = 0;
+		int totalHours = 0;
 
-		for (int days = 0; days < NO_OF_WORKING_DAYS; days++) {
+		for (int days = 0; days < NO_OF_WORKING_DAYS && totalHours < TOTAL_FIXED_WORKING_HOURS; days++) {
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 			switch (empCheck) {
 			case IS_PART_TIME:
 				workHour = 4;
+				totalHours += workHour;
 				break;
 
 			case IS_FULL_TIME:
 				workHour = 8;
+				totalHours += workHour;
+
+				if (totalHours > 100) {
+					totalHours = 100; // To Check whether total hours has exceeded 100 hours limit
+				}
 				break;
 
 			default:
@@ -31,7 +39,7 @@ public class EmployeeWageComputation {
 			totalWage += empWage;
 		}
 
-		System.out.println("Daily Employee Wage: " + totalWage);
+		System.out.println("Monthly Employee Wage: " + totalWage);
 	}
 
 }
