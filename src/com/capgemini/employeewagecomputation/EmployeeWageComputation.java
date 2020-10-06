190e49212cf1c2807dd031e244b1a487c4ee0069
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 16:26:24 +0530

    Added: Ability to check if employee is present or not

--- src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
@@ -1,7 +1,18 @@
 package com.capgemini.employeewagecomputation;
 
 public class EmployeeWageComputation {
+	private static final int IS_FULL_TIME = 1;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		double IS_EMP_PRESENT = Math.floor(Math.random()*10) % 2;
+		
+		if (IS_FULL_TIME == IS_EMP_PRESENT) {
+			System.out.println("Employee is Present");
+		}
+		
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
