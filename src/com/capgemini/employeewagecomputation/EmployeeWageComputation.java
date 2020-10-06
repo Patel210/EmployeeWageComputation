e6523cfa032f3f9a502a0b3d309515010487549e
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 16:44:03 +0530

    Added: Daily wage calculator based on employee presence and type

--- src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
@@ -1,22 +1,23 @@
 package com.capgemini.employeewagecomputation;
 
 public class EmployeeWageComputation {
-	private static final int IS_FULL_TIME = 1;
+	private static final int IS_FULL_TIME = 2;
 	private static final int WAGE_PER_HOUR = 20;
+	private static final int IS_PART_TIME = 1;
+
 	
 	
 	public static void main(String[] args) {
 		
 		int workHour = 0;
-		double IS_EMP_PRESENT = Math.floor(Math.random()*10) % 2;
+		double empCheck = Math.floor(Math.random()*10) % 3;
 		int empWage = 0;
 		
-		if (IS_FULL_TIME == IS_EMP_PRESENT) {
-			System.out.println("Employee is Present");
+		if (IS_FULL_TIME == empCheck) {
 			workHour = 8;
 		}
-		else {
-			System.out.println("Employee is Absent");
+		else if (IS_PART_TIME == empCheck) {
+			workHour = 4;
 		}
 		
 		empWage = workHour * WAGE_PER_HOUR;
