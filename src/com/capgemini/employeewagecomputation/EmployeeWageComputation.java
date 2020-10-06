c9e6e58668c1bcbaa7cbec3540163af6fdec3354
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 17:25:32 +0530

    Added: calculateWage method to calculate employee wage

--- src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
@@ -6,40 +6,46 @@ public class EmployeeWageComputation {
 	private static final int IS_PART_TIME = 1;
 	private static final int NO_OF_WORKING_DAYS = 20;
 	private static final int TOTAL_FIXED_WORKING_HOURS = 100;
+	private static final int HOURS_FULL_TIME = 8;
+	private static final int HOURS_PART_TIME = 4;
 
-	public static void main(String[] args) {
+	/**
+	 * To compute wage
+	 */
+	public int computeWage() {
 
-		int workHour = 0;
-		int empWage = 0;
-		int totalWage = 0;
-		int totalHours = 0;
-
+		int totalWage = 0, totalHours = 0;
 		for (int days = 0; days < NO_OF_WORKING_DAYS && totalHours < TOTAL_FIXED_WORKING_HOURS; days++) {
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 			switch (empCheck) {
 			case IS_PART_TIME:
-				workHour = 4;
-				totalHours += workHour;
-				break;
+				totalHours += HOURS_PART_TIME;
+				totalWage += HOURS_PART_TIME * WAGE_PER_HOUR;
 
-			case IS_FULL_TIME:
-				workHour = 8;
-				totalHours += workHour;
-
-				if (totalHours > 100) {
-					totalHours = 100; // To Check whether total hours has exceeded 100 hours limit
+				if (totalHours > TOTAL_FIXED_WORKING_HOURS) { // To Check whether total hours has exceeded 100 hours
+																// limit
+					totalWage += (totalHours - TOTAL_FIXED_WORKING_HOURS) * WAGE_PER_HOUR;
 				}
 				break;
 
-			default:
-				workHour = 0;
-			}
-			empWage = workHour * WAGE_PER_HOUR;
-			totalWage += empWage;
-		}
+			case IS_FULL_TIME:
+				totalHours += HOURS_FULL_TIME;
+				totalWage += HOURS_FULL_TIME * WAGE_PER_HOUR;
 
-		System.out.println("Monthly Employee Wage: " + totalWage);
+				if (totalHours > TOTAL_FIXED_WORKING_HOURS) { // To Check whether total hours has exceeded 100 hours
+																// limit
+					totalWage += (totalHours - TOTAL_FIXED_WORKING_HOURS) * WAGE_PER_HOUR;
+				}
+				break;
+			}
+		}
+		return totalWage;
+	}
+
+	public static void main(String[] args) {
+		EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation();
+		System.out.println(employeeWageComputation.computeWage());
 	}
 
 }
