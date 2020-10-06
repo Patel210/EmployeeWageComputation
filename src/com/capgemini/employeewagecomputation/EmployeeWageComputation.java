0a837a08e540736dba895c91e89563d2129ab8cb
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 19:08:48 +0530

    Modified: To get Wage details for particular company

--- src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/capgemini/employeewagecomputation/EmployeeWageComputation.java
@@ -8,38 +8,57 @@ public class EmployeeWageComputation implements WageCalculator {
 	private static final int HOURS_FULL_TIME = 8;
 	private static final int HOURS_PART_TIME = 4;
 
-	private ArrayList<Company> company;
-	
-	//Constructor
+	private ArrayList<Company> companies;
+
+	// Constructor
 	public EmployeeWageComputation(ArrayList<Company> companies) {
 		super();
-		this.company = companies;
+		this.companies = companies;
 	}
 
 	/**
-	 * To compute wage
+	 * To compute wage for all companies in the list
 	 */
 	@Override
 	public void computeWage() {
+		for (Company company : companies) {
+			computeWage(company);
+		}
 
-		for (Company company : company) {
+	}
 
+	/**
+	 * @param To compute wage using company Name
+	 */
+	public void computeWageOfParticularCompany(String companyName) {
+		boolean flag = false;
+		while (!flag)
+			for (Company companyObject : companies) {
+				if (companyObject.getCompanyName().equalsIgnoreCase(companyName)) {
+					computeWage(companyObject);
+					flag = true;
+				}
+			}
+	}
+	
+	/**
+	 * @param To compute wage for particular company
+	 */
+	public void computeWage(Company company) {
 			int totalWage = 0, totalHours = 0, noOfDays = 0;
 			for (int days = 0; days < company.getNoOfWorkingDays()
 					&& totalHours < company.getTotalFixedWorkingHours(); days++) {
-				int dailyWage =getDailyWage(company.getWagePerHour());
+				int dailyWage = getDailyWage(company.getWagePerHour());
 				totalWage += dailyWage;
 				totalHours += getDailyHours(company.getWagePerHour());
 				if (totalHours > company.getTotalFixedWorkingHours()) {
-					totalWage -= (totalHours - company.getTotalFixedWorkingHours())
-							* company.getWagePerHour();
+					totalWage -= (totalHours - company.getTotalFixedWorkingHours()) * company.getWagePerHour();
 				}
 				noOfDays++;
-				System.out.println("Day "+ noOfDays + " Emp Wage for company "+ company.getCompanyName() + ": " + dailyWage);
+				System.out.println(
+						"Day " + noOfDays + " Emp Wage for company " + company.getCompanyName() + ": " + dailyWage);
 			}
 			System.out.println("Total Wage of " + company.getCompanyName() + " is: " + totalWage);
-		}
-
 	}
 
 	/**
@@ -81,13 +100,13 @@ public class EmployeeWageComputation implements WageCalculator {
 	public static void main(String[] args) {
 
 		ArrayList<Company> companies = new ArrayList<Company>();
-		
+
 		companies.add(new Company("Codesity", 100, 22, 100));
 		companies.add(new Company("ePW", 90, 20, 120));
 		companies.add(new Company("dMart", 110, 20, 150));
 
 		EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation(companies);
-		employeeWageComputation.computeWage();
+		employeeWageComputation.computeWageOfParticularCompany("dMart");
 
 	}
 
